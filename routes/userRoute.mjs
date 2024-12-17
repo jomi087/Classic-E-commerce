7d8e0a3b6fbec62377407ae1bf244ee5ee32e86a
jomi087 <jomi>
2024-12-17 08:05:11 +0530

    created a landing page route

--- routes/userRoute.mjs
+++ routes/userRoute.mjs
@@ -1,1 +1,10 @@
-module.exports 
+import express from 'express';
+const router = express.Router()
+
+
+import userControllers  from '../controllers/userControllers/userControllers.mjs';
+
+router.get('/',userControllers.landingPage)
+
+
+export default router
