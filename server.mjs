8a686c563290253e2b9e3e049e89df650bed8b1d
jomi087 <jomi>
2024-12-17 08:04:09 +0530

    added  a view engine

--- server.mjs
+++ server.mjs
@@ -8,11 +8,23 @@ dotenv.config();
 import dbConnect from './config/mongoConnect.mjs';
 dbConnect()
 
+//port config
 const port = process.env.serverPort||5000
 
-app.use('')
-app.use()
+//Parser config
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
+
+//view Engine config
+app.set('view engine','ejs');
+
+//view Engine path
+app.set('views', ['./views/user', './views/admin']);
+app.use(express.static('./public'))  
+
+import userRoute from './routes/userRoute.mjs';
+app.use('/',userRoute)
 
 
 app.listen(port,()=>{
